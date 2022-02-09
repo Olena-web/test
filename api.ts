c0da691e0d67ec8c5e1cc011e0eb68dcbee8223a
Olena-web <73849375+Olena-web@users.noreply.github.com>
2022-02-09 07:51:35 +0200

    Add files via upload

---
+++ api.ts
@@ -0,0 +1,195 @@
+import { getItemFromLocalStorage } from './localStorage';
+import {
+  ExistingUserLoginDetails, NewUserDetails, User, UserStatistics, UserWordParameters, Word,
+} from './types';
+
+const base = 'https://rs-lang-mlatysheva.herokuapp.com';
+
+const words = `${base}/words`;
+const users = `${base}/users`;
+const signin = `${base}/signin`;
+
+export async function getWords(group: number, page: number): Promise<Word[]> {
+  const response = (await fetch(`${words}?group=${group}&page=${page}`));
+
+  const data = {
+    items: await response.json(),
+    count: response.headers.get('X-Total-Count'),
+  };
+  // console.log(`data is ${data.items[0]}`);
+  return data.items;
+}
+
+export async function getWord(id: string): Promise<Word> {
+  const response = (await fetch(`${words}/${id}`));
+
+  const word: Word = await response.json();
+  // console.log(`word is ${word.word}`);
+  return word;
+}
+
+export const createUser = async (user: User): Promise<NewUserDetails> => {
+  const rawResponse = await fetch(users, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  });
+  const content = await rawResponse.json();
+  console.log(`in create user content.id is ${content.id}`);
+
+  return content;
+  // assign id: string to new user automatically
+  // {id: '61fa90af95fa390015feec46', email: 'example@google.mail'}
+};
+
+// createUser({ "email": "example@google.mail", "password": "nwekjsf13243" });
+
+export const loginUser = async (user: User): Promise<ExistingUserLoginDetails> => {
+  const rawResponse = await fetch(signin, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  });
+  const content = await rawResponse.json();
+  console.log(`in login user id is ${content.id}`);
+
+  return content;
+  // "message":"Authenticated",
+  // "token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlYzk5M2RmNGNhOWQ2MDAxNzg3NDBhZSIsImlhdCI6MTU5MDI2OTE1OCwiZXhwIjoxNTkwMjgzNTU4fQ.XHKmdY_jk1R7PUbgCZfqH8TxH6XQ0USwPBSKNHMdF6I",
+  // "userId":"5ec993df4ca9d600178740ae"
+};
+
+// loginUser({ "email": "sample@user123.com", "password": "qwerty1234" });
+
+const token = getItemFromLocalStorage('token');
+
+export const createUserWord = async (userId: string, wordId: string, body: UserWordParameters) => {
+  const rawResponse = await fetch(`${users}/${userId}/words/${wordId}`, {
+    method: 'POST',
+    // withCredentials: true,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+  const content = await rawResponse.json();
+
+  console.log(content);
+};
+
+// createUserWord({
+//   userId: "5ec993df4ca9d600178740ae",
+//   wordId: "5e9f5ee35eb9e72bc21af716",
+//   word: { "difficulty": "weak", "optional": {testFieldString: 'test', testFieldBoolean: true} }
+// });
+// Console: {
+//   "id":"5ec9a92acbbd77001736b167",
+//   "difficulty":"weak",
+//   "optional":{
+//     "testFieldString":"test",
+//     "testFieldBoolean":true
+//   },
+//   "wordId":"5e9f5ee35eb9e72bc21af716"
+// }
+
+const getUserWord = async (userId: string, wordId: string) => {
+  const rawResponse = await fetch(`${users}/${userId}/words/${wordId}`, {
+    method: 'GET',
+    // withCredentials: true,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json',
+    },
+  });
+  const content = await rawResponse.json();
+
+  console.log(content);
+};
+
+// getUserWord({
+//   userId: "5ec993df4ca9d600178740ae",
+//   wordId: "5e9f5ee35eb9e72bc21af716"
+// });
+
+// Console: {
+//   "id":"5ec9a92acbbd77001736b167",
+//   "difficulty":"weak",
+//   "optional":{
+//     "testFieldString":"test",
+//     "testFieldBoolean":true
+//   },
+//   "wordId":"5e9f5ee35eb9e72bc21af716"
+// }
+
+export const getUserStatistics = async() => {
+  const userId = getItemFromLocalStorage('id');
+  console.log(`userID is ${userId}`);
+  const token = getItemFromLocalStorage('token');
+  try {
+    const rawResponse = await fetch(`${users}/${userId}/statistics`, {
+      method: 'GET',
+      // withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+      },
+    });  
+    const result = await rawResponse;
+    if (result.status == 200) {
+      return result.json();
+    }
+    if (result.status == 404) {
+      console.log('Ты еще не начал учиться, а уже хочешь статистику смотреть!');
+      return {
+        "learnedWords": 0,
+      }
+    }
+    if (result.status == 401) {
+      console.log('Истек срок действия токена. Зарегистрируйся заново');
+      return {};
+    }
+  }
+  catch(err) {
+    console.log(`err is ${err}`);
+  }  
+}
+
+export const putUserStatistics = async(data: UserStatistics) => {
+  const userId = getItemFromLocalStorage('id');
+  console.log(`userID is ${userId}`);
+  const token = getItemFromLocalStorage('token');
+  try {
+    const rawResponse = await fetch(`${users}/${userId}/statistics`, {
+      method: 'OUT',
+      // withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+      });
+    
+    const result = await rawResponse;
+    if (result.status == 200) {
+      return result.json();
+    }
+    if (result.status == 400) {
+      return 'Bad request';
+    }
+    if (result.status == 401) {
+      return 'Истек срок действия токена. Зарегистрируйся заново';
+    }
+  }
+  catch(err) {
+    console.log(`err is ${err}`);
+  }  
+}
--- removeCard.ts
+++ removeCard.ts
@@ -1,8 +1,8 @@
 import { setItemToLocalStorage, getItemFromLocalStorage } from '../js/localStorage';
 import { firstPage } from './paginationBook';
-import { getWord } from '../js/api';
-import { CardElement} from '../card/cardElement';
-import { Word } from '../js/types';
+import { getWord, createUserWord } from '../js/api';
+import { CardElement } from '../card/cardElement';
+import { UserWordParameters } from '../js/types';
 
 export const deletedCards: Array<string> = [];
 export const difficultWords: Array<string> = [];
@@ -22,39 +22,53 @@ export function removeCard() {
 }
 
 export function difficultWord() {
-  document.body.addEventListener('click', (e) => {
+  document.body.addEventListener('click', async (e) => {
     if (e.target) {
       if ((e.target as HTMLElement).classList.contains('difficult')) {
-        const id = (e.target as HTMLElement).id.split('difficult')[1];
-        const word = document.getElementById(`${id}`);
+        const wordId = (e.target as HTMLElement).id.split('difficult')[1];
+        const word = document.getElementById(`${wordId}`);
         if (word) word.classList.add('difficult-word');
-        difficultWords.push(id);
+        difficultWords.push(wordId);
         setItemToLocalStorage('difficultWords', JSON.stringify(difficultWords));
+        // const difficultWordId: string = (JSON.parse(getItemFromLocalStorage('difficultWords')));
+
+        const myId: string = getItemFromLocalStorage('id');
+        const body: UserWordParameters = {
+          difficulty: 'difficult-word',
+          optional: { testFieldString: 'test', testFieldBoolean: true },
+          // word: {
+          //   difficulty: 'difficult',
+          //   optional: { testFieldString: 'test', testFieldBoolean: true },
+          // },
+        };
+        
+        const newDifficultWord = await createUserWord(myId, wordId, body);
+        console.log(newDifficultWord);
+        // return newDifficultWord;
       }
     }
   });
 }
 
-
 export async function renderDifficultPage() {
-  const cardsOnPage = document.querySelector('book-page');
   document.body.addEventListener('click', async (e) => {
-  if (e.target) {
-      let id = (e.target as HTMLElement).id.split('level')[1];
-      if (id ==='6') {
-  const difficultWordId = getItemFromLocalStorage('difficultWords');
-  console.log(difficultWordId);
-  if (cardsOnPage) cardsOnPage.innerHTML = '';
-        // const data = await getWord(difficultWordId);
-        // const cardOnPage = new CardElement(data).renderCard();
-        // if (cardsOnPage) cardsOnPage.appendChild(cardOnPage);
-        // };
+    const cardsOnPage = document.querySelector('.book-page');
+    if (e.target) {
+      const id = (e.target as HTMLElement).id.split('level')[1];
+      if (id === '6') {
+        const difficultWordId = (JSON.parse(getItemFromLocalStorage('difficultWords')));
+        if (cardsOnPage) cardsOnPage.innerHTML = '';
+        for (let i = 0; i < difficultWordId.length; i += 1) {
+          // eslint-disable-next-line no-await-in-loop
+          const data = await getWord(difficultWordId[i]);
+          const cardOnPage = new CardElement(data).renderCard();
+          if (cardsOnPage) cardsOnPage.appendChild(cardOnPage);
+        }
         return cardsOnPage;
       }
       return cardsOnPage;
     }
-  })}
-  
-
+  });
+}
 
 export default { removeCard, difficultWord, renderDifficultPage };
